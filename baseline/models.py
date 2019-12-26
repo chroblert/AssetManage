d5a0646d4cb7e280155e830c6d3b9c7ef5d8c965
chroblert <jerryzvs@163.com>
2019-12-26 18:24:31 +0800

    Update models.py

--- baseline/models.py
+++ baseline/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class LinuxScanRes(models.Model):
+    publicIP=models.CharField(max_length=30,verbose_name="publicIP")
+    privateIP=models.CharField(max_length=30,verbose_name="privateIP")
+    hostname=models.CharField(max_length=30,verbose_name="hostname")
+    osVersion=models.CharField(max_length=30,verbose_name="osVersion")
+    kernelVersion=models.CharField(max_length=30,verbose_name="kernelVersion")
+
+    tmpIfSeparate=models.BooleanField(default=False, verbose_name="tmpIfSeparate")
+    tmpIfNoexec=models.BooleanField(default=False,verbose_name="tmpIfNoexec")
+    tmpIfNosuid=models.BooleanField(default=False,verbose_name="tmpIfNosuid")
+
+    grubcfgIfExist=models.BooleanField(default=True,verbose_name="grubcfgIfExist")
+    grubcfgPermission=models.CharField(max_length="5",verbose_name="grubcfgPermission")
+    grubcfgIfSetPasswd=models.BooleanField(default=False,verbose_name="grubcfgIfSetPasswd")
+    singleUserModeIfNeedAuth=models.BooleanField(default=False,verbose_name="singleUserModeIfNeedAuth")
+    selinuxState
+
+    def __str__(self):
+        return '%s' % "LinuxScanResult"
